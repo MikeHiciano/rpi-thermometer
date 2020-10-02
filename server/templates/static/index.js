57de17a24a8d365f8316c218b6f3a5c0bbc91351
Miguel Hiciano <mike29hiciano@gmail.com>
2020-10-02 00:14:14 -0400

    making fixes on the js files

--- server/templates/static/index.js
+++ server/templates/static/index.js
@@ -1,6 +1,7 @@
 var url = "/thermometer"
 
 $.getJson(url,function(data){
+    console.log(data.temperature)
     document.getElementById("temperature").innerHTML = data.temperature
     document.getElementById("humidity").innerHTML = data.humidity
 })
