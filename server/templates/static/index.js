139bb9aebcf29c30a8aa1996b4400ecc95eb1ac2
Miguel Hiciano <mike29hiciano@gmail.com>
2020-10-02 00:28:41 -0400

    adding jquery locally

--- server/templates/static/index.js
+++ server/templates/static/index.js
@@ -1,7 +1,7 @@
-var url = "/thermometer"
+var url = "/thermometer";
 
 $.getJson(url,function(data){
-    console.log(data.temperature)
-    document.getElementById("temperature").innerHTML = data.temperature
-    document.getElementById("humidity").innerHTML = data.humidity
-})
+    console.log(data.temperature);
+    document.getElementById("temperature").innerHTML = data.temperature;
+    document.getElementById("humidity").innerHTML = data.humidity;
+});
